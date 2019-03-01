8ec8e915c34732fc9426b024eb1f3900753a4f56
s4704 <igorkud00@gmail.com>
2019-03-01 19:17:44 +0300

    Android.mk: prima wlan symlinks
    Change-Id: Ieed884aeabbef9335179b5eef18e8e28a3a9b004

--- Android.mk
+++ Android.mk
@@ -18,4 +18,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter m1721,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# PRIMA symlinks
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima)
+$(shell ln -sf /persist/WCNSS_wlan_dictionary.dat $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat)
+$(shell ln -sf /persist/WCNSS_qcom_wlan_nv.bin $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin)
+$(shell ln -sf /persist/WCNSS_qcom_wlan_nv.bin $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv_boot.bin)
+$(shell ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+
 endif
