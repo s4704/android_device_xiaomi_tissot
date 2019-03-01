2bb668f528fcc72887386700d8b99b3c0fc8f5ad
s4704 <igorkud00@gmail.com>
2019-03-01 23:50:20 +0300

    Android.mk: update symlink wlan
    Change-Id: I57db5d7f5a50a5591ac4242ff5b07f8329b1cadf

--- Android.mk
+++ Android.mk
@@ -23,5 +23,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima)
 $(shell ln -sf /persist/WCNSS_wlan_dictionary.dat $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat)
 $(shell ln -sf /persist/WCNSS_qcom_wlan_nv.bin $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin)
+$(shell ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 endif
