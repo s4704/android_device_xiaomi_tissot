f10c154d56d44695fdb35498752d42cc94e48a71
s4704 <igorkud00@gmail.com>
2019-03-01 19:14:27 +0300

    BoardConfig.mk: update wifi flags
    Change-Id: I7b2d5b9b0fdb4f023f5389b2d4298f615514461b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,20 +213,18 @@ BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := dsp firmware persist
 
-# Wi-Fi
+# Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
 BOARD_WLAN_DEVICE := qcwcn
-WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 -include vendor/meizu/m1721/BoardConfigVendor.mk
